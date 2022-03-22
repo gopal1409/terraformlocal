10881208fc6555e0c8e43750372594fe6460d420
gopal das <gopal1409@gmail.com>
2022-03-22 19:03:34 +0530

    local tags

--- c5.resource-group.tf
+++ c5.resource-group.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "rg" {
   name = "${local.resource_name_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
   location = var.resource_group_location
+  tags = local.common_tags
 }
