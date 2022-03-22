fea844603d9e856f365750d923c4261ef4a9fa43
gopal das <gopal1409@gmail.com>
2022-03-22 19:53:11 +0530

    vnet

---
+++ c6-1-vnet-input-variables.tf
@@ -0,0 +1,49 @@
+#virtual network
+variable "vnet_name" {
+  type = string
+  default = "vnet-default"
+}
+variable "vnet_address_space" {
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "web_subnet_name" {
+type = string
+default = "websubnet"
+}
+
+variable "web_subnet_address" {
+    type = list(string)
+    default = ["10.0.1.0/24"]
+}
+variable "app_subnet_name" {
+type = string
+default = "appsubnet"
+}
+
+variable "app_subnet_address" {
+    type = list(string)
+    default = ["10.0.11.0/24"]
+}
+
+variable "db_subnet_name" {
+type = string
+default = "dbsubnet"
+}
+
+variable "db_subnet_address" {
+    type = list(string)
+    default = ["10.0.21.0/24"]
+
+}
+
+variable "bastion_subnet_name" {
+type = string
+default = "bastionsubnet"
+}
+
+variable "bastion_subnet_address" {
+    type = list(string)
+    default = ["10.0.1.0/24"]
+}
